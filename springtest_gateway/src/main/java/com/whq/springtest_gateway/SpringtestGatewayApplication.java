665fc8959d92ac65715b14d33bcd98a1fe8b3a53
wanghq-c <wanghq-c@glodon.com>
2019-05-10 10:00:18 +0800

    add global hystrix

--- springtest_gateway/src/main/java/com/whq/springtest_gateway/SpringtestGatewayApplication.java
+++ springtest_gateway/src/main/java/com/whq/springtest_gateway/SpringtestGatewayApplication.java
@@ -5,11 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.server.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 @Configuration
+@RestController
 public class SpringtestGatewayApplication {
 
     public static void main(String[] args) {
@@ -22,6 +27,15 @@ public class SpringtestGatewayApplication {
                 .route(RequestPredicates.GET("/hi")
                                 .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
                         hiHandler::Hi);
+    }
+
+
+    @RequestMapping(value = "/fallbackcontroller")
+    public Map<String, String> fallBackController() {
+        Map<String, String> res = new HashMap();
+        res.put("code", "-100");
+        res.put("data", "service not available");
+        return res;
     }
 }
 
